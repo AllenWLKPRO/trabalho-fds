36804262c0320d7fddcf703534ebc11a07c06217
Danilo Marchesano <danilopro.marchesano2022@gmail.com>
2025-07-07 23:04:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarmensagem() {
+    alert("Ninguém começa no topo do mundo. Nem eu, nem você, nem mesmo Deus... Mas o vazio insuportável do trono do céu acabou. De agora em diante, eu estarei sentado nele!");
+}
